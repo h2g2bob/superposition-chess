2b87c81e367807946d14476256521856dba90aa8
David Batley <git@dbatley.com>
2022-01-23 19:53:20 +0000

    Make Square a functional component

--- web/src/Square.jsx
+++ web/src/Square.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import PropTypes from 'prop-types';
@@ -6,24 +5,21 @@ import React from 'react';
 import ImageMixer from './ImageMixer';
 import './Square.css';
 
-class Square extends React.Component {
-  render() {
-    const {
-      team, pieces, background, isSelected, onClick,
-    } = this.props;
-    return (
-      <span
-        className={`square square-${background} square-selected-${isSelected}`}
-        onClick={onClick}
-        role="button"
-      >
-        <ImageMixer
-          team={team}
-          pieces={pieces}
-        />
-      </span>
-    );
-  }
+function Square({
+  team, pieces, background, isSelected, onClick,
+}) {
+  return (
+    <span
+      className={`square square-${background} square-selected-${isSelected}`}
+      onClick={onClick}
+      role="button"
+    >
+      <ImageMixer
+        team={team}
+        pieces={pieces}
+      />
+    </span>
+  );
 }
 
 Square.propTypes = {
