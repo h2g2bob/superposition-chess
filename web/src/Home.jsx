2ed3a1cd7237d36d14817f50bc7dc0f7740570f7
David Batley <git@dbatley.com>
2022-02-13 20:41:33 +0000

    Change url when we click "new game"
    We don't really _use_ this for anything, but it does work!

--- web/src/Home.jsx
+++ web/src/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import uuid from 'react-uuid';
 import { useDispatch } from 'react-redux';
+import { useRouter } from 'react-router5';
 import { newGameAction } from './actions';
 
 function Home() {
   const dispatch = useDispatch();
+  const router = useRouter();
   return (
     <button
       type="button"
       onClick={() => {
         const newGameId = uuid();
+        router.navigate('game', { id: newGameId });
         dispatch(newGameAction(newGameId, 4));
       }}
     >
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,4 +1,4 @@
 export default [
   { name: 'home', path: '/' },
-  { name: 'game', path: '/game' },
+  { name: 'game', path: '/game/:id' },
 ];
